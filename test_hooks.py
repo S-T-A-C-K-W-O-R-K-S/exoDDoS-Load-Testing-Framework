e44b9e0e1337f6b20d1a7e5278070e1dc368ff0f
Vlad-Alexandru Tănăsescu <vlad-tanasescu@outlook.com>
2019-12-11 11:25:34 +0000

    Create ENV Object & Correct Session Token Scope

--- test_hooks.py
+++ test_hooks.py
@@ -1,31 +1,34 @@
-import json
+class Environment:
+    def __init__(self, client_key, is_oh):
+        self.client_key = client_key
+        self.is_oh = is_oh
 
 
-session_token = None
-
-client_key = "api-brl"
-is_oh = "true"
+env = Environment("api-brl", "true")
 
 
-def login(self, username, password): # on_start
-    response = self.client.post(f"/auth/login?client-key={client_key}&is-oh={is_oh}", json = {"username":f"{username}","password":f"{password}"})
+def login(self, username, password):  # on_start
+
+    response = self.client.post(f"/auth/login?client-key={env.client_key}&is-oh={env.is_oh}", json={"username": f"{username}", "password": f"{password}"})
 
     if response.json()['status'] == "ok":
         session_token = response.json()['result']['Token']
         print(f"logged in with username '{username}' and password '{password}'")
         print(f"session token: {session_token}")
+        return session_token
 
     else:
         print(f"login status: {response.json()['status']}")
 
 
-def logout(self, username): # on_stop
-    response = self.client.post(f"/auth/logout?client-key={client_key}&is-oh={is_oh}&token={session_token}")
-    
+def logout(self, username, session_token):  # on_stop
+
+    response = self.client.post(f"/auth/logout?client-key={env.client_key}&is-oh={env.is_oh}&token={session_token}")
+
     if response.json()['status'] == "ok":
         print(f"{username} has logged out")
         print(f"session '{session_token}' has been terminated")
-        
+
     else:
         print(f"logout status: {response.json()['status']}")
 
