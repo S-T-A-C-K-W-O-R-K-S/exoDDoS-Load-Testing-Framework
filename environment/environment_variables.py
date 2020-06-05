d0338fb878259a3bf6563a3f0f1355af632a027c
Vlad-Alexandru Tănăsescu <vlad-tanasescu@outlook.com>
2020-06-05 19:23:54 +0100

    Make The Credentials And The User Pool Environment Variables

--- environment/environment_variables.py
+++ environment/environment_variables.py
@@ -1,8 +1,14 @@
+import os
+
 class env:
 
     debug_mode = False
 
+    credentials = "environment" + os.path.sep + "user_credentials.csv"
     host = "CBPM"
+    user_pool = None
 
-    def __init__(self, host):
+    def __init__(self, credentials, host, user_pool):
+        self.credentials = credentials
         self.host = host
+        self.user_pool = user_pool
