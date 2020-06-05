8161d2411bb353a638ffc94c09300686e06ff6b9
Vlad-Alexandru Tănăsescu <vlad-tanasescu@outlook.com>
2020-06-05 19:25:01 +0100

    Update Request Failure Hook To Work With Locust v1.0.3

--- hooks/locust_on_request_failure.py
+++ hooks/locust_on_request_failure.py
@@ -1,26 +1,10 @@
 from imports.environment import env
-from locust.events import request_failure
+from locust import events
 
 
-def on_failure(request_type, name, response_time, exception, **kwargs):
+@events.request_failure.add_listener
+def on_request_failure(request_type, name, response_time, exception, **kwargs):
     if env.debug_mode:
         print(f"[DEBUG] :: Exception {request_type} Request URL: {exception.request.url}")
         print(f"[DEBUG] :: Exception Response Code: {exception.response.status_code}")
         print(f"[DEBUG] :: Exception Response Message: {exception.response.json()['Message']}")
-
-request_failure += on_failure
-
-
-                                # # # # # # # # # # # # # # # # #
-                                #                               #
-                                #   ORDER OF EVENTS             #
-                                #                               #
-                                #       1. Locust SETUP         #
-                                #       2. TaskSet SETUP        #
-                                #       3. TaskSet ON_START     #
-                                #       4. TaskSet TASKS...     #
-                                #       5. TaskSet ON_STOP      #
-                                #       6. TaskSet TEARDOWN     #
-                                #       7. Locust TEARDOWN      #
-                                #                               #
-                                # # # # # # # # # # # # # # # # #
