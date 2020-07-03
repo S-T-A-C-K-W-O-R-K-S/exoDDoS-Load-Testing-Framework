6bc9f1ac2558d48d81677b2b5e33da46ddf06ca1
Vlad-Alexandru Tănăsescu <vlad-tanasescu@outlook.com>
2020-07-03 17:51:50 +0100

    Update Hooks

--- hooks/locust_on_init.py
+++ hooks/locust_on_init.py
@@ -1,20 +1,31 @@
-from datetime import datetime
-from locust import events
-from locust.runners import STATE_STOPPING, STATE_STOPPED, STATE_CLEANUP, WorkerRunner
-import gevent, time
+import gevent, sys, time
+
+from locust import events, runners
+from locust.runners import STATE_CLEANUP, STATE_STOPPING, STATE_STOPPED, WorkerRunner
 
 
 def checker(environment):
-    while not environment.runner.state in [STATE_STOPPING, STATE_STOPPED, STATE_CLEANUP]:
+    while not environment.runner.state in [STATE_CLEANUP, STATE_STOPPING, STATE_STOPPED]:
         time.sleep(1)
         if environment.runner.stats.total.fail_ratio > 0.2:  # 20%
             print("------------------------------------------------------------------------------------------------------------------------------------------------------")
             print(f"Test Run Will Terminate Prematurely Due To A Failure Rate Of {environment.runner.stats.total.fail_ratio}")
             print()
+
             environment.runner.quit()
+            sys.exit()
+
+            # TODO: Initiate Shutdown & Remove RUNNER.QUIT() And SYS.EXIT() & Remove SYS Import
+            # LOCUST.MAIN.SHUTDOWN()
+
             return
+
+#    if environment.runner.state == STATE_STOPPED:
+#        if exoddos.shutdown_on_stop:
+#           TODO: Initiate Shutdown
+
 
 @events.init.add_listener
 def on_init(environment, **_kwargs):
-    if not isinstance(environment.runner, WorkerRunner):
+    if not isinstance(environment.runner, WorkerRunner):  # evaluates to true if the runner is either LocalRunner or MasterRunner
         gevent.spawn(checker, environment)
--- hooks/locust_on_request_failure.py
+++ hooks/locust_on_request_failure.py
@@ -1,10 +1,10 @@
-from imports.environment import env
+from environment.exoddos import exoddos
 from locust import events
 
 
 @events.request_failure.add_listener
 def on_request_failure(request_type, name, response_time, exception, **kwargs):
-    if env.debug_mode:
+    if exoddos.debug_mode:
         print(f"[DEBUG] :: Exception {request_type} Request URL: {exception.request.url}")
         print(f"[DEBUG] :: Exception Response Code: {exception.response.status_code}")
         print(f"[DEBUG] :: Exception Response Message: {exception.response.json()['Message']}")
