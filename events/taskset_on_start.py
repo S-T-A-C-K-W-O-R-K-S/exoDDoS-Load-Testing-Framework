562db45aa9a139d8f2a8b8bc4722ce493f8e4412
Vlad-Alexandru Tănăsescu <vlad-tanasescu@outlook.com>
2020-07-03 17:51:28 +0100

    Update Events

--- events/taskset_on_start.py
+++ events/taskset_on_start.py
@@ -1,4 +1,4 @@
-from imports.environment import env
+from environment.exoddos import exoddos
 
 
 def on_start_setup(self, username, password):
@@ -11,16 +11,16 @@ def on_start_setup(self, username, password):
                 session_id = response.json()["SessionID"]
                 session_cookie = response.cookies[".AspNet.ApplicationCookie"]
 
-                if env.debug_mode:
-                    print(f"[DEBUG] :: User '{username}' Has Logged In With Password '{password}'")
+                if exoddos.debug_mode:
+                    print(f"[DEBUG] :: Session ID NO_SESSION: User '{username}' Has Logged In With Password '{password}' And", end = " ")
                 else:
-                    print(f"User '{username}' Has Logged In")
+                    print(f"Session ID NO_SESSION: User '{username}' Has Logged In And", end = " ")
 
                 print(f"Session ID '{session_id}' Has Been Created")
 
             else:
                 session_id = "NO_SESSION"
-                print(f"Logging In Has Failed With Error Code {response.status_code}")
+                print(f"Session ID NO_SESSION: Logging In As '{username}' Has Failed With Error Code {response.status_code}")
 
     with self.client.post(f"/auth/disclaimer",
         headers={"session-id": f"{session_id}", "Cookie": f".AspNet.ApplicationCookie={session_cookie}"},
@@ -30,8 +30,11 @@ def on_start_setup(self, username, password):
                 session_cookie = response.cookies[".AspNet.ApplicationCookie"]
                 print(f"Session ID {session_id}: User '{username}' Has Accepted The Disclaimer")
                 
-                if env.debug_mode:
-                    print(f"[DEBUG] :: Session Cookie: {session_cookie}")
+                if exoddos.debug_mode:
+                    print(f"[DEBUG] :: Session ID {session_id} Cookie: {session_cookie}")
+
+            else:
+                print(f"Session ID {session_id}: Accepting The Disclaimer Has Failed With Error Code {response.status_code}")
 
     with self.client.get(f"/home/configuration",
         headers={"Cookie": f".AspNet.ApplicationCookie={session_cookie}"},
@@ -48,6 +51,6 @@ def on_start_setup(self, username, password):
                 print(f"Session ID {session_id}: User '{username}' With ID '{user_id}' Is In Collaboration ID '{user_collaboration_id}'")
 
             else:
-                print(f"Retrieving The Application Configuration Has Failed With Error Code {response.status_code}")
+                print(f"Session ID {session_id}: Retrieving The Application Configuration Has Failed With Error Code {response.status_code}")
 
     return {"session_id": f"{session_id}", "session_cookie": f"{session_cookie}", "user_collaboration_id": f"{user_collaboration_id}"}
--- events/taskset_on_stop.py
+++ events/taskset_on_stop.py
@@ -1,6 +1,3 @@
-from imports.environment import env
-
-
 def on_stop_teardown(self, username, session_id, session_cookie):
 
     with self.client.get(f"/auth/logout",
@@ -8,7 +5,7 @@ def on_stop_teardown(self, username, session_id, session_cookie):
         catch_response=True) as response:
 
             if response.status_code == 200:
-                print(f"Session ID {session_id}: User '{username}' Has Logged Out And The Session Has Been terminated")
+                print(f"Session ID {session_id}: User '{username}' Has Logged Out And Session ID '{session_id}' Has Been Terminated")
 
             else:
-                print(f"Logging Out Has Failed With Error Code {response.status_code}")
+                print(f"Session ID {session_id}: Logging Out As '{username}' In Session ID '{session_id}' Has Failed With Error Code {response.status_code}")
