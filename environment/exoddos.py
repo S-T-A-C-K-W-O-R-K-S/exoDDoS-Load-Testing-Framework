88990ac9d0dbc16b48c600475ea763e01d379461
Vlad-Alexandru Tănăsescu <vlad-tanasescu@outlook.com>
2020-07-03 17:53:34 +0100

    Update Environment & Add New Environment Variable For Future Use

--- environment/exoddos.py
+++ environment/exoddos.py
@@ -1,20 +1,21 @@
 import os
 
-class env:
+class exoddos:
 
     debug_mode = False
-    
+    shutdown_on_stop = True
+
+    env = None
+    host = "CBPm"
     credentials = "environment" + os.path.sep + "user_credentials.csv"
-
-    host = "CBPM"
-
     user_pool = []
 
     if os.environ.get("debug-user") is not None and os.environ.get("debug-pass") is not None:
         debug_user = [os.environ.get("debug-user"), os.environ.get("debug-pass")]
         user_pool.append(debug_user)
 
-    def __init__(self, credentials, host, user_pool):
+    def __init__(self, env, host, credentials, user_pool):
+        self.env = env
         self.credentials = credentials
         self.host = host
         self.user_pool = user_pool
