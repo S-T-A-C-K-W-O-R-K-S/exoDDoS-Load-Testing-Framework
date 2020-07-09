ba5e3ce5b626127dbbc97491d214086d84f234e7
Vlad-Alexandru Tănăsescu <vlad-tanasescu@outlook.com>
2020-07-09 17:53:25 +0100

    Remove Unnecessary Flag & Add New Flag For Retrying Failed Logins

--- environment/exoddos.py
+++ environment/exoddos.py
@@ -2,10 +2,10 @@ import os
 
 class exoddos:
 
-    debug_mode = False
-    shutdown_on_stop = True
+    debug_mode = False              # when enabled, more verbose console output will be generated
+    recycle_failed_logins = False   # when enabled, credentials for users that fail to log in will go back in the user pool
+    shutdown_on_stop = True         # when enabled, the framework will shut down automatically at the end of the test run
 
-    env = None
     host = "CBPm"
     credentials = "environment" + os.path.sep + "user_credentials.csv"
     user_pool = []
@@ -14,8 +14,7 @@ class exoddos:
         debug_user = [os.environ.get("debug-user"), os.environ.get("debug-pass")]
         user_pool.append(debug_user)
 
-    def __init__(self, env, host, credentials, user_pool):
-        self.env = env
+    def __init__(self, host, credentials, user_pool):
         self.credentials = credentials
         self.host = host
         self.user_pool = user_pool
