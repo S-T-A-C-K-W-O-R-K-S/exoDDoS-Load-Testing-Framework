7b9eb6779edc9f2bd8d7eee4e23127dfe6c2c8f6
Vlad-Alexandru Tănăsescu <vlad-tanasescu@outlook.com>
2020-07-13 22:32:38 +0100

    Add New Environment Variable & Remove INIT And Old Environment Variable

--- environment/exoddos.py
+++ environment/exoddos.py
@@ -1,10 +1,11 @@
 import os
 
+
 class exoddos:
 
     debug_mode = False              # when enabled, more verbose console output will be generated
     recycle_failed_logins = False   # when enabled, credentials for users that fail to log in will go back in the user pool
-    shutdown_on_stop = True         # when enabled, the framework will shut down automatically at the end of the test run
+    results = 100                   # the maximum number of results to return when performing an expensive request
 
     host = "CBPm"
     credentials = "environment" + os.path.sep + "user_credentials.csv"
@@ -13,8 +14,3 @@ class exoddos:
     if os.environ.get("debug-user") is not None and os.environ.get("debug-pass") is not None:
         debug_user = [os.environ.get("debug-user"), os.environ.get("debug-pass")]
         user_pool.append(debug_user)
-
-    def __init__(self, host, credentials, user_pool):
-        self.credentials = credentials
-        self.host = host
-        self.user_pool = user_pool
