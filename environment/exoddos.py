10a21e2997131015edf0a0f0642435ab4ea9d39f
Vlad-Alexandru Tănăsescu <vlad-tanasescu@outlook.com>
2020-08-11 18:26:19 +0100

    Remove Sensitive Information From Environment File

--- environment/exoddos.py
+++ environment/exoddos.py
@@ -5,16 +5,16 @@ class exoddos:
 
     debug_mode = False  # when enabled, more verbose console output will be generated
 
-    host = "https://csdwd260.ecbt1.tadnet.net/csdb-idqm/api"
+    host = "CSDB"
 
-    ssl_certificate = "environment" + os.path.sep + "ssl" + os.path.sep + "csdb-fin.crt"
-    ssl_key = "environment" + os.path.sep + "ssl" + os.path.sep + "csdb-fin-decrypted.key"
+    ssl_certificate = "environment" + os.path.sep + "ssl" + os.path.sep + "placeholder_ssl_certificate"
+    ssl_key = "environment" + os.path.sep + "ssl" + os.path.sep + "placeholder_decrypted_ssl_key"
     ssl = (ssl_certificate, ssl_key)
 
     query_pages = [(1,100), (101,200), (201,300), (301,400), (401,500)]
 
     user_pool = [
-        ["EUFINCORE", "vlad.tanasescu@fincore.com", "APPR_CSDB_BA-ADMIN"],
-        ["EUGERGANA", "gergana.tabakova@fincore.com", "APPR_CSDB_BA-ADMIN"],
-        ["EURADICMI", "mina.radic@fincore.com", "APPR_CSDB_BA-ADMIN"]
+        ["DDOS-00001", "DDOS-00001@EXO.DDOS", "CSDB-ADMIN"],
+        ["DDOS-00002", "DDOS-00002@EXO.DDOS", "CSDB-ADMIN"],
+        ["DDOS-00003", "DDOS-00003@EXO.DDOS", "CSDB-ADMIN"]
     ]
