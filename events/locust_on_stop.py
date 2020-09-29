d7f67869441b1f2d8f5275339584521bce02800c
Vlad-Alexandru Tănăsescu <vlad-tanasescu@outlook.com>
2020-09-29 21:27:42 +0100

    Minor Additions To Console Output

--- events/locust_on_stop.py
+++ events/locust_on_stop.py
@@ -8,16 +8,20 @@ def on_stop_teardown(self):
     if exoddos.debug_mode:
         print("[DEBUG] :: Cookie Header: " + self.cookie_header)
 
-    with self.client.post("/logout",
-        name="Authentication > IAM Logout",
-        headers={"X-ESCB-IAM-CC": "EU", "X-ESCB-IAM-UID": f"{self.username}", "X-ESCB-IAM-EMAIL": f"{self.email}",
-            "X-ESCB-IAM-ROLES": f"{self.role}", "Cookie": f"{self.cookie_header}", "Content-Type": "text/plain"},
-        cert=exoddos.ssl, verify=False,
-        json={"user": f"{self.user_id}", "session": f"{self.session_id}", "databaseid": "db1"},
-        catch_response=True) as response:
+    if self.session_id == "NO_SESSION":
+        print(utility.timestamp(self) + f"________NO_SESSION: User '{self.username}' Without Session Has Been Preemptively Terminated")
 
-            if response.status_code == 200:
-                print(utility.timestamp(self) + f"Session ID {self.session_id}: User '{self.username}' Has Logged Out And Session ID '{self.session_id}' Has Been Terminated")
+    else:
+        with self.client.post("/logout",
+            name="Authentication > IAM Logout",
+            headers={"X-ESCB-IAM-CC": "EU", "X-ESCB-IAM-UID": f"{self.username}", "X-ESCB-IAM-EMAIL": f"{self.email}",
+                "X-ESCB-IAM-ROLES": f"{self.role}", "Cookie": f"{self.cookie_header}", "Content-Type": "text/plain"},
+            cert=exoddos.ssl, verify=False,
+            json={"user": f"{self.user_id}", "session": f"{self.session_id}", "databaseid": "db1"},
+            catch_response=True) as response:
 
-            else:
-                print(utility.timestamp(self) + f"Session ID {self.session_id}: Logging Out As '{self.username}' In Session ID '{self.session_id}' Has Failed With Error Code {response.status_code}")
+                if response.status_code == 200:
+                    print(utility.timestamp(self) + f"Session ID {self.session_id}: User '{self.username}' Has Logged Out And Session ID '{self.session_id}' Has Been Terminated")
+
+                else:
+                    print(utility.timestamp(self) + f"Session ID {self.session_id}: Logging Out As '{self.username}' In Session ID '{self.session_id}' Has Failed With Error Code {response.status_code}")
