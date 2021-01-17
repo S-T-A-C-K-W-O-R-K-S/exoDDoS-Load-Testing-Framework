a84ef1d43eae28a102de931f0df6a977902f3a51
Vlad-Alexandru Tănăsescu <vlad-tanasescu@outlook.com>
2021-01-17 16:36:20 +0000

    Minor Tweaks Here And There

--- environment/exoddos.py
+++ environment/exoddos.py
@@ -5,7 +5,7 @@ class exoddos:
 
     debug_mode = False  # when enabled, more verbose console output will be generated
 
-    host = "http://ebowd83.ecbt1.tadnet.net:8080/api"
+    host = "ESTR"
 
     credentials = "environment" + os.path.sep + "user_credentials.csv"
 
